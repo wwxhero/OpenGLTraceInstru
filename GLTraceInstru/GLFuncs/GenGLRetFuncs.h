e23dc3ba1eec4b002f8195f87cd2300695e5ca98
wanxwang <wanxin-wang@uiowa.edu>
2018-11-21 19:14:08 -0600

    seperate glfunctions parser into a dll

---
+++ GLTraceInstru/GLFuncs/GenGLRetFuncs.h
@@ -0,0 +1,37 @@
+#pragma once
+#ifndef _GLTRACE_INJECTOR_H
+#define _GLTRACE_INJECTOR_H
+#include <list>
+#ifndef GLFUNCS_EXPORTS
+#define FUNCS_API __declspec(dllimport)
+#else
+#define FUNCS_API __declspec(dllexport)
+#endif
+typedef struct Func_t
+{
+	const char* retType[2];
+	const char* callConven[2];
+	const char* funcName[2];
+	typedef struct Param_t
+	{
+		const char* paramType[2];
+		const char* paramName[2];
+	} Param;
+	Param* params;
+	unsigned int numParams;
+} Func;
+
+typedef struct MemSrc_t
+{
+	const char* p;
+	unsigned int size;
+	HANDLE hFile;
+	HANDLE hMapFile;
+} MemSrc;
+
+FUNCS_API void StartParse4Funcs(const char* szPath, MemSrc* mem, std::list<Func*>& lstFuncs);
+FUNCS_API void GenFuncsDecl(const char* szPath, const MemSrc* mem, const std::list<Func*>& lstFuncs);
+FUNCS_API void GenFuncsImpl(const char* szPath, const MemSrc* mem, const std::list<Func*>& lstFuncs);
+FUNCS_API void EndParse4Funcs(MemSrc* mem, std::list<Func*>& lstFuncs);
+
+#endif
