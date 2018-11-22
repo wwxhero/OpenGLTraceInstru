526b8474df1cff01f410300c86edc842d61ff339
wanxwang <wanxin-wang@uiowa.edu>
2018-11-21 19:45:05 -0600

    UI prototype for injector

--- GLTraceInstru/GLTraceInstru/Resource.h
+++ GLTraceInstru/GLTraceInstru/Resource.h
@@ -32,18 +32,21 @@
 #define IDC_CHKTRACKSELECT              1005
 #define IDC_CHKFULLROWSELECT            1006
 #define IDC_OPTIONS                     1007
-#define IDC_CHECK1                      1008
 #define IDC_CHKHASBUTTONS               1008
-#define IDC_CHKHIERARCHY                1009
 #define IDC_EDTSRCDIR                   1009
 #define IDC_BTNDIR                      1010
 #define IDC_SRC_PATH                    1011
-#define IDC_PROGRESS1                   1012
 #define IDC_PROGINJECTION               1012
 #define IDC_BTNSTART                    1013
+#define IDC_EDTGLHEADER                 1014
+#define IDC_BTNGLHEADER                 1015
+#define IDC_SRC_HEADER                  1016
+#define IDC_EDTSRCDIR_LOGGER            1020
+#define IDC_BTNLOGGERFUNCGEN            1021
+#define IDC_LOGGER_SRC_PATH             1022
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        141
